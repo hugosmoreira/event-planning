87898b0f0a7a2a4b5cfdf02102c10db14fa0b96a
hugosmoreira <hugo.n4@gmail.com>
2022-06-24 23:05:48 -0700

    update components

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Logo } from './Logo'
+
+type Props = {}
+
+const Header = (props: Props) => {
+  return (
+    <div>
+        <Logo />
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Lesson.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Lesson = (props: Props) => {
+  return (
+    <div>Lesson</div>
+  )
+}
+
+export default Lesson
