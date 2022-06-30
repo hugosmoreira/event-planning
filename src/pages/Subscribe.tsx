84f3738df5cb0fd6ed4761c7131db49450cc921f
hugosmoreira <hugo.n4@gmail.com>
2022-06-29 17:59:06 -0700

    subscribe page finalizar

---
+++ src/pages/Subscribe.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+type Props = {}
+
+const Subscribe = (props: Props) => {
+  return (
+    <div className='min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center'>
+        <div className='max-w-[1100px] flex items-center justify-between mt-20 mx-auto'>
+            <div className='max-w-[640px]'>
+
+            </div>
+            <div className='p-8 bg-gray-700 border border-gray-500 rounded'>
+                <strong className='text-2xl mb-6 block'>Inscreva-se gratuitamente</strong>
+            </div>
+
+        </div>
+            <img src="" alt="" />
+    </div>
+  )
+}
+
+export default Subscribe
